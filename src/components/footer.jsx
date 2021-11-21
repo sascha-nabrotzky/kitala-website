02c9a84188d14a37d4c818405210b14d41bca976
Sascha <68822358+sascha-nabrotzky@users.noreply.github.com>
2021-11-21 12:23:07 +0100

    Add link to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import * as footerStyles from '../components/footer.module.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import * as footerStyles from "../components/footer.module.scss";
 
 function Footer() {
+  return (
+    <>
+      <footer>
+        <ul className={footerStyles.footerListItems}>
+          <li className={footerStyles.footerLink}>
+            <Link to="/impressum">Impressum</Link>
+          </li>
+          <li className={footerStyles.footerLink}>
+            <Link to="/datenschutz">Datenschutz</Link>
+          </li>
+          <li className={footerStyles.footerLink}>
+            <a
+              href="https://sascha-nabrotzky.github.io"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Web-Entwicklung | Sascha Nabrotzky
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </>
+  );
+}
 
-    return (
-  
-      <>
-          <footer>
-            <ul className={footerStyles.footerListItems}>
-                <li className={footerStyles.footerLink}>
-                    <Link to="/impressum">Impressum</Link>
-                  </li>
-                <li className={footerStyles.footerLink}>
-                  <Link to="/datenschutz">Datenschutz</Link>
-                </li>
-            </ul>
-          </footer>
-
-            
-      </>
-     
-    );
-  }
-  
-  export default Footer;
+export default Footer;
