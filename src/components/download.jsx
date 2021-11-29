90d0760ca25d5bc1fb011ed7baf4b46807b9a2ee
Sascha <68822358+sascha-nabrotzky@users.noreply.github.com>
2021-11-29 20:56:50 +0100

    [TASK] Add pdf

--- src/components/download.jsx
+++ src/components/download.jsx
@@ -1,32 +1,59 @@
-import React from 'react';
-import * as downloadStyles from '../components/download.module.scss';
-import MitgliedsantragPDF from '../images/Mitgliedsantrag_interaktiv.pdf';
-import iconPDF from '../images/pdf.svg';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import * as downloadStyles from "../components/download.module.scss";
+import MitgliedsantragPDF from "../images/Mitgliedsantrag_interaktiv.pdf";
+import MitgliedsantragFoerderPDF from "../images/Mitgliedsantrag_Foerdermitglied_interaktiv.pdf";
+import iconPDF from "../images/pdf.svg";
+import { Helmet } from "react-helmet";
 
 function Download() {
-
-    return (
-      
-      <>
+  return (
+    <>
       <Helmet>
-          <title>Downloads | KiTaLa e.V.</title>
-          <meta name='description' content='Wichtige Dokumente können hier heruntergeladen werden' />
+        <title>Downloads | KiTaLa e.V.</title>
+        <meta
+          name="description"
+          content="Wichtige Dokumente können hier heruntergeladen werden"
+        />
       </Helmet>
 
       <main className={downloadStyles.downMain}>
         <h1>Downloads</h1>
         <h2>Wichtige Dokumente und mehr</h2>
         <a href={MitgliedsantragPDF} target="_blank" rel="noopener noreferrer">
-          <img className={downloadStyles.pdficon} src={iconPDF} alt="PDF Icon" /> Mitgliedsantrag
+          <img
+            className={downloadStyles.pdficon}
+            src={iconPDF}
+            alt="PDF Icon"
+          />{" "}
+          Mitgliedsantrag
         </a>
-        <a href='https://www.kreis-steinfurt.de/kv_steinfurt/Ressourcen/Familie,%20Kinder%20&%20Jugendliche/Elternbeitr%C3%A4ge%20f%C3%BCr%20das%20aktuelle%20Kindergartenjahr.pdf' target="_blank" rel="noopener noreferrer">
-          <img className={downloadStyles.pdficon} src={iconPDF} alt="PDF Icon" /> Elternbeiträge
+        <a
+          href={MitgliedsantragFoerderPDF}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className={downloadStyles.pdficon}
+            src={iconPDF}
+            alt="PDF Icon"
+          />{" "}
+          Mitgliedsantrag Fördermitglied
+        </a>
+        <a
+          href="https://www.kreis-steinfurt.de/kv_steinfurt/Ressourcen/Familie,%20Kinder%20&%20Jugendliche/Elternbeitr%C3%A4ge%20f%C3%BCr%20das%20aktuelle%20Kindergartenjahr.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className={downloadStyles.pdficon}
+            src={iconPDF}
+            alt="PDF Icon"
+          />{" "}
+          Elternbeiträge
         </a>
       </main>
-      </>
-     
-    );
-  }
-  
-  export default Download;
+    </>
+  );
+}
+
+export default Download;
