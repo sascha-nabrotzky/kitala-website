dfac685ae8cfc370ca1e5fcfd45d0038f2ecfdb8
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-10-13 19:55:04 +0200

    remove react

--- src/components/InterviewQA.tsx
+++ src/components/InterviewQA.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import { useState, useRef } from "react";
 import Chevron from "./chevron";
 import style from "../styles/interviewQA.module.css";
 
--- src/components/ToTopButton.tsx
+++ src/components/ToTopButton.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import styles from "../styles/ToTopButton.module.css";
 
 function ToTopButton() {
